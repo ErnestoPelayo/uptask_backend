0f2a0daa2e52e0e048c38661eb4d8e9d68ebe5d4
ErnestoPelayo <42719997+ErnestoPelayo@users.noreply.github.com>
2024-07-23 15:43:22 -0700

    project raouter added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from 'dotenv'
 import { connectDb } from "./config/db"
+import projectRoutes from './routes/projectRoutes'
 
 dotenv.config()
 
@@ -8,5 +9,6 @@ connectDb()
 
 const app = express()
 
+app.use('/api/projects',projectRoutes)
 
 export default app
