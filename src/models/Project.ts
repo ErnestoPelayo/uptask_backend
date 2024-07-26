fec89370fb20b1cc6b14198a5968d60397b505e6
ErnestoPelayo <42719997+ErnestoPelayo@users.noreply.github.com>
2024-07-26 13:06:35 -0700

    Task Model added

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,9 +1,11 @@
-import mongoose,{Schema,Document} from "mongoose";
+import mongoose,{Schema,Document,PopulatedDoc, Types} from "mongoose";
+import { ITask } from "./Task";
 
 export interface IProyect extends Document  {
     projectName:string
     clientName:string
     description:string 
+    tasks : PopulatedDoc<ITask & Document>[]
 }
 
 const ProjectSchema : Schema = new Schema({
@@ -21,8 +23,14 @@ const ProjectSchema : Schema = new Schema({
         type:String,
         trim:true,
         required:true
-    }
-})
+    },
+    tasks : [
+        {
+            type:Types.ObjectId,
+            ref:'Task'
+        }
+    ]
+},{timestamps:true})
 
 const Project = mongoose.model<IProyect>("Project", ProjectSchema)
 
