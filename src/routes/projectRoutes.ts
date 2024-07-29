87293a971bbf11a20637b037bfa16218554a3509
ErnestoPelayo <42719997+ErnestoPelayo@users.noreply.github.com>
2024-07-29 12:56:59 -0700

    project middleware added, project validation

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -4,6 +4,7 @@ import { body, param } from "express-validator"
 import { ProjectController } from "../controllers/ProjectController"
 import {handleInputErrors} from '../middleware/validation'
 import { TaskController } from "../controllers/TaskController"
+import { validationProject } from "../middleware/proyect"
 const router = Router()
 
 router.post('/',
@@ -35,7 +36,18 @@ router.delete('/:id',
 
 /* Tasks */         
 router.post('/:id_Project/task',
-        param('id').isMongoId().withMessage('Id Invalido'),
+        param('id_Project').isMongoId().withMessage('Id Invalido'),
+        validationProject,
+        body('name').notEmpty().withMessage("El Nombre de la tarea"),
+        body('description').notEmpty().withMessage("La descripcion de la tarea es obligatorio"),    
+        TaskController.createTask
+)
+
+router.post('/task',
+        param('id_Project').isMongoId().withMessage('Id Invalido'),
+        validationProject,
+        body('name').notEmpty().withMessage("El Nombre de la tarea"),
+        body('description').notEmpty().withMessage("La descripcion de la tarea es obligatorio"),    
         TaskController.createTask
 )
 
